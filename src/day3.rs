7a9ed2aa753efd70d8db354dc4987b31b6bdf9da
Jitrayu Punrattanapongs <jitrayu.p@gmail.com>
2023-12-21 21:26:58 +0000

    feat(add-solution-for-day3-part2): part 2 solution

--- src/day3.rs
+++ src/day3.rs
@@ -1,11 +1,15 @@
-use std::{char, collections::HashSet, fs, usize};
+use std::{
+    char,
+    collections::{HashMap, HashSet},
+    fs, usize,
+};
 
 pub fn aoc2023day3() {
     let input = fs::read_to_string("inputs/day3.txt").unwrap();
 
     println!("The results of Advent of Code 2023 Day3:");
     println!("    Part I:  {}", part1(&input));
-    // println!("    Part II: {}", part2(&input));
+    println!("    Part II: {}", part2(&input));
     println!("-------------------------------------------");
 }
 
@@ -59,10 +63,11 @@ fn part1(input: &str) -> u32 {
     nums.iter().map(|ele| ele.parse::<u32>().unwrap()).sum()
 }
 
-#[allow(dead_code)]
 fn part2(input: &str) -> u32 {
     let rs: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let mut sym_pos = HashSet::new();
+    let mut sym_count: HashMap<(&i32, &i32), HashSet<(i32, i32)>> = HashMap::new();
+    let mut nums = Vec::new();
     // looking for a position of the symbols
     for (i, line) in rs.iter().enumerate() {
         for (j, ch) in line.iter().enumerate() {
@@ -71,7 +76,51 @@ fn part2(input: &str) -> u32 {
             }
         }
     }
-    0
+    for (i, j) in &sym_pos {
+        for row in i - 1..=i + 1 {
+            for col in j - 1..=j + 1 {
+                if row >= 0
+                    && row <= rs[0].len() as i32
+                    && col >= 0
+                    && col <= rs[0].len() as i32
+                    && rs[row as usize][col as usize].is_ascii_digit()
+                {
+                    let mut ncol = col;
+                    let mut cur = (row, col);
+                    while ncol >= 0 && rs[row as usize][ncol as usize].is_ascii_digit() {
+                        cur = (row, ncol);
+                        ncol -= 1;
+                    }
+                    sym_count
+                        .entry((i, j))
+                        .or_insert_with(HashSet::new)
+                        .insert(cur);
+                    // .push(cur);
+                }
+            }
+        }
+    }
+    // println!("{:?}", &sym_count);
+    let num_pos: Vec<HashSet<(i32, i32)>> =
+        sym_count.into_values().filter(|v| v.len() == 2).collect();
+    // println!("{:?}", &num_pos);
+    for gear in num_pos {
+        let mut str_num: Vec<String> = Vec::new();
+        for (i, j) in gear {
+            let mut cur = String::from("");
+            let mut ncol = j;
+            while ncol < rs[0].len() as i32 && rs[i as usize][ncol as usize].is_ascii_digit() {
+                cur.push(rs[i as usize][ncol as usize]);
+                ncol += 1;
+            }
+            str_num.push(cur);
+        }
+        nums.push(str_num);
+    }
+    // nums.iter().map(|ele| ele.parse::<u32>().unwrap()).sum()
+    nums.iter()
+        .map(|v| v[1].parse::<u32>().unwrap() * v[0].parse::<u32>().unwrap())
+        .sum()
 }
 
 #[cfg(test)]
